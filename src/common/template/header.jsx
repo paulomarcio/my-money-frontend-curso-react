1a932e9b40a86a8685c35cee4d329deea78890bf
Paulo Márcio <paulom.matias@gmail.com>
2019-05-20 01:14:19 -0300

    [enh] Done Router component using HashHistory.

--- src/common/template/header.jsx
+++ src/common/template/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
     <header className='main-header'>
-        <a href='#' className='logo'>
+        <a href='/#' className='logo'>
             <span className='logo-mini'><b>My</b>M</span>
             <span className='logo-lg'>
                 <i className='fa fa-money'></i>

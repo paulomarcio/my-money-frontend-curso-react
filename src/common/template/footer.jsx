e88913f6ba344c757b5e365ecab7f30aea8c42bd
Paulo Márcio <paulom.matias@gmail.com>
2019-05-20 00:56:49 -0300

    [enh] Done Footer component.

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>Copyright &copy; 2019 <a href="http://www.cod3r.com.br" target="_blank">Cod3r</a></strong>
+    </footer>
+)
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,10 +4,15 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer />
     </div>
 )

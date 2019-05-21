62dfade39e6909efb9b7c689eec7569711be8062
Paulo Marcio <paulom.matias@gmail.com>
2019-05-21 11:58:11 -0300

    Fixing wrong sidebar complement name in require statement.

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import '../common/template/dependencies'
 
 import Header from '../common/template/header'
-import SideBar from '../common/template/sideBar'
+import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
 

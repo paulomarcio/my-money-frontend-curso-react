e603fb70bd1004344145dd8594a33b902f965cc6
Paulo Márcio <paulom.matias@gmail.com>
2019-05-21 22:30:29 -0300

    [enh] Done dashboard actions for dashboard reducers with promise middleware.

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+
+    componentWillMount() {
+        this.props.getSummary()
+    }
+
     render() {
         const { credit, debt } = this.props.summary
 
@@ -26,4 +33,5 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({summary: state.dashboard.summary})
-export default connect(mapStateToProps)(Dashboard)
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)

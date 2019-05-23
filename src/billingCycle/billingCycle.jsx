04bb325fb63bb18d9114e96fbd1973078214120b
Paulo Marcio <paulom.matias@gmail.com>
2019-05-23 14:52:44 -0300

    [enh] Done tabContent component and billingCycle component redux integration.

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from "redux";
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -6,8 +8,15 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+import { selectTab } from "../common/tab/tabActions";
 
 class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+    }
+
     render() {
         return (
             <div>
@@ -20,7 +29,20 @@ class BillingCycle extends Component {
                             <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
-                        <TabsContent />
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <h1>Listar</h1>
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <h1>Incluir</h1>
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <h1>Atualizar</h1>
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                                <h1>Excluir</h1>
+                            </TabContent>
+                        </TabsContent>
                     </Tabs>
                 </Content>
             </div>
@@ -28,4 +50,5 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)

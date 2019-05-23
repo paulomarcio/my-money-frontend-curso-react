7248068b76ba9b3431b4b0076384d6699df5eb4d
Paulo Marcio <paulom.matias@gmail.com>
2019-05-23 10:33:55 -0300

    [enh] Done tabHeader component interacts with redux

--- src/common/tab/tabHeader.jsx
+++ src/common/tab/tabHeader.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { selectTab } from './tabActions'
 
 class TabHeader extends Component {
     render() {
+        const selected = this.props.tab.selected === this.props.target
         return (
-            <li>
-                <a href='javascript:;' data-toggle='tab' data-target={this.props.target}>
+            <li className={selected ? 'active' : ''}>
+                <a href='javascript:;' data-toggle='tab' data-target={this.props.target} onClick={() => this.props.selectTab(this.props.target)}>
                     <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                 </a>
             </li>
@@ -12,4 +17,6 @@ class TabHeader extends Component {
     }
 }
 
-export default TabHeader
+const mapStateToProps = state => ({tab : state.tab})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)

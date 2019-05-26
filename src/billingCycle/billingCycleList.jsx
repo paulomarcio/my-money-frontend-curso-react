67a3e836780540efb9bc084ed37982231cabf02c
Paulo Marcio <paulom.matias@gmail.com>
2019-05-26 19:52:53 -0300

    [enh] Done billingCycleList component listing data into view.

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList } from './billingCycleActions'
 
 class BillingCycleList extends Component {
+
+    componentWillMount() {
+        this.props.getList()
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render() {
         return (
             <div>
@@ -12,11 +31,15 @@ class BillingCycleList extends Component {
                         <th>Ano</th>
                     </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        {this.renderRows()}
+                    </tbody>
                 </table>
             </div>
         )
     }
 }
 
-export default BillingCycleList
+const mapStateToProps = state => ({list: state.billingCycle.list})
+const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)

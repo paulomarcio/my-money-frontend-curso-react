02db2a8d3625680a170ca808e336f001cb9f7c38
Paulo Marcio <paulom.matias@gmail.com>
2019-05-31 11:53:47 -0300

    [enh] Done error and success messages configuration

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,6 +13,12 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
     return {
         type: 'TEMP'
     }
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -6,6 +6,7 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 export default props => (
     <div className="wrapper">
@@ -15,5 +16,6 @@ export default props => (
             <Routes />
         </div>
         <Footer />
+        <Messages />
     </div>
 )

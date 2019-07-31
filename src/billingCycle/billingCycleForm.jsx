56f2447b840d6512a7251ed5d1000a1d6d0ffa13
Paulo Márcio <paulom.matias@gmail.com>
2019-07-31 00:32:20 -0300

    [enh] Added summary calculate function

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -6,16 +6,27 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary()
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
                     <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} readOnly={readOnly} type='number' label='Mês' cols='12 4' placeholder='Informe o mês' />
                     <Field name='year' component={LabelAndInput} readOnly={readOnly} type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' showStatus={true} />
                 </div>

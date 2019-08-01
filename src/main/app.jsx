0a1098da91d554ee0b195436f76a874a2362167f
Paulo Marcio <paulom.matias@gmail.com>
2019-08-01 16:05:29 -0300

    [enh] Refactoring Router component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 import '../common/template/dependencies'
+import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
-import Routes from './routes'
 import Messages from '../common/msg/messages'
 
 export default props => (
@@ -13,7 +11,7 @@ export default props => (
         <Header />
         <SideBar />
         <div className="content-wrapper">
-            <Routes />
+            {props.children}
         </div>
         <Footer />
         <Messages />
